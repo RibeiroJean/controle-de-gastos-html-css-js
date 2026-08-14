258c9e28c06b7fd988fbe47de5b02c712b73ecb9
Jean <jean.enrique.ribeiro@escola.pr.gov.br>
2026-08-14 16:12:43 -0300

    Configura Firebase com minhas chaves

--- firebase-init.js
+++ firebase-init.js
@@ -1,9 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCBSl2BdAd_DghcW5a5-qp8gFcK0nfuGCY",
-    authDomain: "controle-de-gastos-3e7f9.firebaseapp.com",
-    projectId: "controle-de-gastos-3e7f9",
-    storageBucket: "controle-de-gastos-3e7f9.appspot.com",
-    messagingSenderId: "1018055774945",
-    appId: "1:1018055774945:web:bb0b24d7c0a4992e16dda0"
+  apiKey: "AIzaSyC3UqV-NIIfksGKRs_amjyP7iJ1oPfMoHI",
+  authDomain: "controle-de-gastos-73593.firebaseapp.com",
+  projectId: "controle-de-gastos-73593",
+  storageBucket: "controle-de-gastos-73593.firebasestorage.app",
+  messagingSenderId: "311275117974",
+  appId: "1:311275117974:web:1374234494c29ffc8d11b7",
+  measurementId: "G-24WBE1ZB9E"
 };
+
 firebase.initializeApp(firebaseConfig);
