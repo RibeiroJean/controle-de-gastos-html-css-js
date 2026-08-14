28adab608df6e86bf522f9513ae853eb10c92889
Jean <jean.enrique.ribeiro@escola.pr.gov.br>
2026-08-14 16:36:56 -0300

    Usando Firestore direto em vez da API localhost

--- services/transaction.service.js
+++ services/transaction.service.js
@@ -1,57 +1,57 @@
 const transactionService = {
-    findByUser: () => {
-        return callApi({
-            method: "GET",
-            url: "http://localhost:3000/transactions"
-        })
+    findByUser: user => {
+        return firebase.firestore()
+            .collection('transactions')
+            .where('user.uid', '==', user.uid)
+            .orderBy('date', 'desc')
+            .get()
+            .then(snapshot => {
+                return snapshot.docs.map(doc => ({
+                    ...doc.data(),
+                    uid: doc.id
+                }));
+            });
     },
+
     findByUid: uid => {
-        return callApi({
-            method: "GET",
-            url: `http://localhost:3000/transactions/${uid}`
-        })
-    },
-    remove: transaction => {
-        return callApi({
-            method: "DELETE",
-            url: `http://localhost:3000/transactions/${transaction.uid}`
-        })
-    },
-    save: transaction => {
-        return callApi({
-            method: "POST",
-            url: "http://localhost:3000/transactions",
-            params: transaction
-        })
-    },
-    update: transaction => {
-        return callApi({
-            method: "PATCH",
-            url: `http://localhost:3000/transactions/${transaction.uid}`,
-            params: transaction
-        })
-    }
-}
-
-function callApi({method, url, params}) {
-    return new Promise(async (resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-
-        xhr.open(method, url, true);
-        xhr.setRequestHeader('Authorization', await firebase.auth().currentUser.getIdToken())
-        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4) {
-                const json = JSON.parse(this.responseText);
-                if (this.status != 200) {
-                    reject(json);
-                } else {
-                    resolve(json);
+        return firebase.firestore()
+            .collection('transactions')
+            .doc(uid)
+            .get()
+            .then(doc => {
+                if (doc.exists) {
+                    return {
+                        ...doc.data(),
+                        uid: doc.id
+                    };
                 }
-            }
-        };
+                return null;
+            });
+    },
 
-        xhr.send(JSON.stringify(params));
-    })
+    remove: transaction => {
+        return firebase.firestore()
+            .collection('transactions')
+            .doc(transaction.uid)
+            .delete();
+    },
+
+    save: transaction => {
+        return firebase.firestore()
+            .collection('transactions')
+            .add(transaction);
+    },
+
+    update: transaction => {
+        return firebase.firestore()
+            .collection('transactions')
+            .doc(transaction.uid)
+            .update({
+                type: transaction.type,
+                date: transaction.date,
+                money: transaction.money,
+                transactionType: transaction.transactionType,
+                description: transaction.description
+            });
+    }
 }
